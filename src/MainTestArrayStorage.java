0199661d41d9ca9e183523ac3b9c355682735d6f
kalashnikovio <aboima01@gmail.com>
2023-01-17 20:14:43 +0300

    Выполнение задание 2 урока 17.01.2023

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -22,8 +22,7 @@ public class MainTestArrayStorage {
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
-        System.out.print("UPD dummy: ");
-        System.out.println(ARRAY_STORAGE.update(r1));
+        System.out.println("UPD Uuid: " + ARRAY_STORAGE.update(r1));
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
